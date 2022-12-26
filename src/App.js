d0e407a657c6bdb535cc2d20aa229ca2fc1a75c7
Bhavin Rasiklal Borkhataria <bhavin.rb@sciafrica.net>
2022-12-26 21:36:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react"
+import Navbar from "./components/navbar";
+import Main from "./components/main";
 import './App.css';
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="container">
+          <Navbar 
+              darkMode={darkMode} 
+              toggleDarkMode={toggleDarkMode}
+          />
+          <Main darkMode={darkMode} />
+      </div>
+  
   );
 }
 
